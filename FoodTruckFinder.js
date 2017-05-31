869b5de060e4d90919dd204132e431c508031d39
KellyPT <trankphuong@gmail.com>
2017-05-30 22:34:22 -0700

    Retrieve the first 10 data rows

--- FoodTruckFinder.js
+++ FoodTruckFinder.js
@@ -1,8 +1,35 @@
-var request = require('request');
-
-request('http://data.sfgov.org/resource/bbb8-hzi6.json', function (error, response, body) {
-  console.log(body);
-});
+// var request = require('request');
+//
+// request('http://data.sfgov.org/resource/bbb8-hzi6.json', function (error, response, body) {
+//   console.log(body);
+// });
 
 // to run:
 // $ npm install request && node FoodTruckFinder.js
+
+var request = require('request');
+var url = require('url');
+
+var options = {
+    protocol: "http:",
+    host: "data.sfgov.org",
+    pathname: "/resource/bbb8-hzi6.json",
+    search: "$query=SELECT applicant, location LIMIT 10"
+};
+
+var dataURL = url.format(options);
+
+request(dataURL, function(error, response, body){
+    if (error) {
+        console.log("Failed to load business IDs from API");
+        process.exit(1);
+      }
+    var results = JSON.parse(body);
+    console.log(results);
+});
+
+// Applicant
+// PermitLocation
+// start24 <= Now <= end24
+// DayOfWeek
+// sorted alphabetically
